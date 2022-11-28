da37e3888c72ff9e7b2014da8bb7f43c6f95984e
Qodesmith <theqodesmith@gmail.com>
2022-11-28 11:48:33 -0500

    Added print styles.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   const [year, setYear] = useState<number>(() => new Date().getFullYear())
 
   return (
-    <div className="py-8">
+    <div className="container mx-auto py-8">
       <header className="prose max-w-none pb-8 text-center dark:prose-invert">
         <h1 className="mb-2">Calendar Year {year}</h1>
         <YearInputSection onYearChange={setYear} />
