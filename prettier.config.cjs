7038baef120c5471ae0dd929c01b586db91fe89b
Qodesmith <theqodesmith@gmail.com>
2022-11-28 08:51:55 -0500

    Removed boilerplate code. Fixed config files.

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: false,
+}
